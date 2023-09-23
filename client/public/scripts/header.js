6902278f8d1b5261a347f7c0132ebd1f474c0337
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-09-23 01:56:30 -0400

    relayout header

--- client/public/scripts/header.js
+++ client/public/scripts/header.js
@@ -3,21 +3,20 @@ const headerContainer = document.createElement("div");
 headerContainer.className = "header-container";
 
 const headerLeft = document.createElement("div");
-headerLeft.className = "header-left";
+headerLeft.className = "header-section header-left";
 
 const headerLogo = document.createElement("img");
 headerLogo.className = "header-logo";
 headerLogo.src = "/wtaelite.png";
 
-const headerTitle = document.createElement("h1");
-headerTitle.className = "header-title";
-headerTitle.textContent = "Current Top 10 Players";
+const headerTitle = document.createElement("div");
+headerTitle.className = "header-section header-title";
+headerTitle.textContent = "Top 10 Players";
 
 headerLeft.appendChild(headerLogo);
-headerLeft.appendChild(headerTitle);
 
 const headerRight = document.createElement("div");
-headerRight.className = "header-right";
+headerRight.className = "header-section header-right";
 
 const headerButton = document.createElement("Button");
 headerButton.className = "button";
@@ -30,6 +29,7 @@ headerButton.addEventListener("click", function handleClick(event) {
 headerRight.appendChild(headerButton);
 
 headerContainer.appendChild(headerLeft);
+headerContainer.appendChild(headerTitle);
 headerContainer.appendChild(headerRight);
 
 header.appendChild(headerContainer);
