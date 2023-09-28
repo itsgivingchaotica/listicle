2e706248d20df7e583c74b9e73db73a05bf9fcd0
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-09-28 14:56:44 -0400

    add controller to route

--- server/routes/players.js
+++ server/routes/players.js
@@ -1,17 +1,15 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import playersData from "../data/players.js";
+import PlayersController from "../controllers/players.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json(playersData);
-});
+router.get("/", PlayersController.getPlayers);
 
-router.get("/:giftId", (req, res) => {
+router.get("/:playerId", (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, "../public/player.html"));
 });
 
