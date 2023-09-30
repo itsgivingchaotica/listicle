73a56444fd7e3694efe7314f3396c46a0b83b3c8
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-09-29 20:56:36 -0400

    rebuild with search bar

--- server/public/scripts/players.js
+++ server/public/scripts/players.js
@@ -1,9 +1,53 @@
 import { getCountryCode } from "./countryCodes.js";
 
+//to store all data from the server
+let data;
+
+const playerCards = document.querySelectorAll("card");
+
+const filterPlayers = (searchTerm) => {
+  playerCards.forEach((card) => {
+    const playerInfo = JSON.parse(card.dataset.playerInfo);
+    console.log(playerInfo);
+
+    //convert all player object values to lowercase for case-insensitive search
+
+    const playerValues = Object.values(playerInfo).map((value) =>
+      value.toString().toLowerCase()
+    );
+
+    console.log(playerValues);
+
+    //check to see if any value contains the search term
+
+    const terms = searchTerm.toLowerCase().split(" ");
+    const matches = terms.some((term) =>
+      playerValues.some((value) => value.includes(term))
+    );
+
+    console.log(terms, "term");
+    console.log(matches, "matches");
+
+    if (matches) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+};
+
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("input", (event) => {
+  const searchTerm = event.target.value.trim();
+  console.log(searchTerm);
+  filterPlayers(searchTerm);
+});
+
 const renderPlayers = async () => {
   const response = await fetch("/players");
   console.log(response);
-  const data = await response.json();
+  data = await response.json();
   console.log(data);
   const mainContent = document.getElementById("main-content");
 
@@ -11,6 +55,9 @@ const renderPlayers = async () => {
     data.map((player) => {
       const card = document.createElement("div");
       card.classList.add("card");
+
+      // Store the player object as a data attribute
+      card.dataset.playerInfo = JSON.stringify(player);
 
       const topContainer = document.createElement("div");
       topContainer.classList.add("top-container");
